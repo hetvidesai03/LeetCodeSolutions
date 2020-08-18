3ef35a8574b117203b238c4bce6cdc389f2b492a
Hetvi Desai <dhetvi@ymail.com>
2020-08-17 22:41:47 -0400

    Difficulty: Medium

---
+++ Rotate Image.py
@@ -0,0 +1,8 @@
+import numpy as np
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        matrix.reverse()
+        matrix[:]= np.transpose(matrix).tolist()
