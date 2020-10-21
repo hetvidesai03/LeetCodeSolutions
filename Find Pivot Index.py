ebf863c6f84044e43de6511ef540fc135c5a9699
Hetvi Desai <dhetvi@ymail.com>
2020-10-21 16:24:16 -0400

    Difficulty: Easy

---
+++ Find Pivot Index.py
@@ -0,0 +1,8 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        
+        for index in range(len(nums)):
+            if sum(nums[:index])==sum(nums[index+1:]):
+                return index
+            
+        return -1
